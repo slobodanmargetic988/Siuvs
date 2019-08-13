d0270958c5fa7b4147abe15db0a47e70261826f9
slobodanmargetic988 <slobodanmargetic988@gmail.com>
2019-08-13 17:18:15 +0200

    added first batch of files for search support
    added files
    controller\SearchController.java
    model\Search.java
    css\siuvs.css
    templates\results.html
    added a form to:
    fragments\navigation.html
    which creates a search bar on all pages and redirects to results page pasing hte search argiment to view model

---
+++ src/main/java/org/bitbucket/pbosko/siuvs/controller/SearchController.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.bitbucket.pbosko.siuvs.controller;
+
+import javax.persistence.Entity;
+import javax.servlet.http.HttpServletRequest;
+import org.bitbucket.pbosko.siuvs.model.Search;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.view.RedirectView;
+
+/**
+ *
+ * @author Slobodan
+ */
+
+@Controller
+public class SearchController {
+
+
+
+    @PostMapping("/search-results")
+    public String performSearch(final Model model, @RequestParam(name = "upit") String upit ) {
+        model.addAttribute("query", upit );
+        return "results"; //view
+    }
+    
+  /*
+    @RequestMapping(value = "message", method = RequestMethod.GET)
+    public String messages(Model model) {
+        model.addAttribute("messages", messageRepository.findAll());
+        return "message/list";
+    }
+    */
+}

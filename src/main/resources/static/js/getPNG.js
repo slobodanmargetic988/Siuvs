ca51e4e697170c55733e0a291a8d904f214ec663
slobodanmargetic988 <slobodanmargetic988@gmail.com>
2019-08-17 13:22:59 +0200

    edited the background of elements in getpng and getpdf scripts to make files nicer

--- src/main/resources/static/js/getPNG.js
+++ src/main/resources/static/js/getPNG.js
@@ -1,6 +1,6 @@
 
 function getPNG() {
-
+//html2canvas does not have a save file function like jsPDF so we have to make one
     function saveAs(uri, filename) {
         var link = document.createElement('a');
         if (typeof link.download === 'string') {
@@ -20,55 +20,44 @@ function getPNG() {
         }
     }
 
-    //first locate element 0 (the first and only) with the class type px-content which is located on every page. then set its width to 1200 in order to make a pdf
+    //first locate element 0 (the first and only) with the class type px-content which is located on every page. 
+    //then set its width to 1200 in order to make a pdf and background color to white to make pdf look nicer
     var downloadwhat = "px-content";
     var content = document.getElementsByClassName(downloadwhat);
     if (typeof content[0] != "undefined") {
         content[0].style.width = "1200px";
-    }
+       content[0].style.backgroundColor = "white";}
 
+    var table= document.getElementsByTagName("tbody");
+    if (typeof table[0] != "undefined") {table[0].style.backgroundColor = "white";}
 
-    // second make a list of all obcets with "panel" class and make them a little bit narower than px-content so they dont look bad inside the PDF
+    // second make a list of all obcets with "panel" and panel-body class and make them a little bit narower 
+    // than px-content so they dont look bad inside the PDF
     var panel = document.getElementsByClassName("panel");
-    if (typeof panel[0] != "undefined") {
-        panel[0].style.width = "1150px";
-    }
-    if (typeof panel[1] != "undefined") {
-        panel[1].style.width = "1150px";
-    }
-    if (typeof panel[2] != "undefined") {
-        panel[2].style.width = "1150px";
-    }
-    if (typeof panel[3] != "undefined") {
-        panel[3].style.width = "1150px";
-    }
+    var panelb = document.getElementsByClassName("panel-body");
+     for (var i = 0; i < 6; i++) {
+    if (typeof panel[i] != "undefined") {panel[i].style.width = "1150px";
+        panel[i].style.backgroundColor = "white";}
+      if (typeof panelb[i] != "undefined") {panelb[i].style.width = "1150px";
+        panelb[i].style.backgroundColor = "white";}  
+     }
 
 
 
     //set Canvas_height to 1400 because pxcontent is now 1200 so it will definitly fit. 
     var Canvas_Width = 1200;// $("#"+downloadwhat).width();
-    var Canvas_Height = $("." + downloadwhat).height();
-    var margin = 15;
+    var Canvas_Height = $("." + downloadwhat).height()+20;
+
 
     //make PDF a little bigger than canvas so it all fits
     var PDF_Width = Canvas_Width + 30;
-    var PDF_Height = PDF_Width * 1.2 + 30;
-    var totalPDFPages = Math.ceil(Canvas_Height / 1400) - 1; //this is used if we want to set PDF height for making multiple page pdf instead of single page pdf
+
     console.log("pravi se canvas:");
     //capture everithing inside the div with class px-content and set it as canvas 
     html2canvas($("." + downloadwhat)[0], {allowTaint: true, imageTimeout: 0, width: Canvas_Width, height: Canvas_Height}).then(function (canvas) {
 
         var uri = canvas.toDataURL(allowtaint = true);
-        /*
-         var pdf = new jsPDF('p', 'pt',  [PDF_Width, PDF_Height]);        
-         
-         pdf.addImage(uri,  margin, margin, Canvas_Width, Canvas_Height,  'NONE', 0);
-         for (var i = 1; i <= totalPDFPages; i++) { 
-         pdf.addPage(PDF_Width,PDF_Height);
-         pdf.addImage(uri,  margin, -PDF_Height*i+margin, Canvas_Width, Canvas_Height,  'NONE', 0);
-         }
-         
-         pdf.save("HTML-Document.pdf");*/
+
         saveAs(uri, 'canvas.png');
 
     });
--- src/main/resources/static/js/getPdf.js
+++ src/main/resources/static/js/getPdf.js
@@ -1,52 +1,34 @@
 
 function getPDF() {
-    /*
-     function saveAs(uri, filename) {
-     var link = document.createElement('a');
-     if (typeof link.download === 'string') {
-     link.href = uri;
-     link.download = filename;
-     
-     //Firefox requires the link to be in the body
-     document.body.appendChild(link);
-     
-     //simulate click
-     link.click();
-     
-     //remove the link when done
-     document.body.removeChild(link);
-     } else {
-     window.open(uri);
-     }
-     }
-     
-     */
-    //first locate element 0 (the first and only) with the class type px-content which is located on every page. then set its width to 1200 in order to make a pdf
+
+    //first locate element 0 (the first and only) with the class type px-content which is located on every page. 
+    //then set its width to 1200 in order to make a pdf and background color to white to make pdf look nicer
     var downloadwhat = "px-content";
     var content = document.getElementsByClassName(downloadwhat);
     if (typeof content[0] != "undefined") {
         content[0].style.width = "1200px";
+       content[0].style.backgroundColor = "white";
+
     }
+    var table= document.getElementsByTagName("tbody");
+    if (typeof table[0] != "undefined") {table[0].style.backgroundColor = "white";}
 
 
-    // second make a list of all obcets with "panel" class and make them a little bit narower than px-content so they dont look bad inside the PDF
+    // second make a list of all obcets with "panel" and panel-body class and make them a little bit narower 
+    // than px-content so they dont look bad inside the PDF
     var panel = document.getElementsByClassName("panel");
-    if (typeof panel[0] != "undefined") {
-        panel[0].style.width = "1160px";
-    }
-    if (typeof panel[1] != "undefined") {
-        panel[1].style.width = "1160px";
-    }
-    if (typeof panel[2] != "undefined") {
-        panel[2].style.width = "1160px";
-    }
-    if (typeof panel[3] != "undefined") {
-        panel[3].style.width = "1160px";
-    }
+    var panelb = document.getElementsByClassName("panel-body");
+     for (var i = 0; i <6; i++) {
+    if (typeof panel[i] != "undefined") {panel[i].style.width = "1150px";
+        panel[i].style.backgroundColor = "white";}
+      if (typeof panelb[i] != "undefined") {panelb[i].style.width = "1150px";
+        panelb[i].style.backgroundColor = "white";}  
+     }
+       
 
 
 
-    //set Canvas_height to 1400 because pxcontent is now 1200 so it will definitly fit. 
+    //set Canvas_height to 14200 because pxcontent is now 1200 so it will definitly fit. 
     var Canvas_Width = 1200;// $("#"+downloadwhat).width();
     var Canvas_Height = $("." + downloadwhat).height();
     var margin = 15;
@@ -55,7 +37,7 @@ function getPDF() {
     var PDF_Width = Canvas_Width + 30;
     var PDF_Height = PDF_Width * 1.2 + 30;
     var totalPDFPages = Math.ceil(Canvas_Height / 1400) - 1; //this is used if we want to set PDF height for making multiple page pdf instead of single page pdf
-    //var meta =document.getElementById("viewport").setAttribute("content", "initial-scale=2");
+
     console.log("pravi se canvas:");
     //capture everithing inside the div with class px-content and set it as canvas 
     html2canvas($("." + downloadwhat)[0], {allowTaint: true, imageTimeout: 0, width: Canvas_Width, height: Canvas_Height}).then(function (canvas) {
@@ -71,7 +53,7 @@ function getPDF() {
         }
 
         pdf.save("HTML-Document.pdf");
-        //saveAs(uri, 'canvas.png');
+      
 
     });
 

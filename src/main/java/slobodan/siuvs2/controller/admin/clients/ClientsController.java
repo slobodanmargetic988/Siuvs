9ab81540f70a24bd90e9fc59081bb792a63d23a2
slobodanmargetic988 <slobodanmargetic988@gmail.com>
2020-05-06 19:02:17 +0200

    prva vrlo stabilna verzija siuvs2
    prva vrlo stabilna verzija siuvs2
    klijenti unos i izmena sredjeni da ne bacaju errore razne

--- src/main/java/slobodan/siuvs2/controller/admin/clients/ClientsController.java
+++ src/main/java/slobodan/siuvs2/controller/admin/clients/ClientsController.java
@@ -63,32 +63,47 @@ public class ClientsController {
     }
 
     @GetMapping(value = "/clients/new")
-    public String createForm() {
+    public String createForm(final Model model) {
+        List<Opstina> listaopstina= new ArrayList<Opstina>();
+        listaopstina=opstinaService.findAllOrderByNameAsc();
+         model.addAttribute("listaopstina", listaopstina);
         return "admin/clients/new";
     }
 
     @PostMapping(value = "/clients/new")
     public String create(
-            @Valid @ModelAttribute("newClient") final Client newClient,
-            final BindingResult bindingResult,
+           
+            @RequestParam(name = "LSG") OpstinaID opstina,
+            @RequestParam(name = "name") String name,
+         
             final RedirectAttributes redirectAttributes
     ) {
-        if (bindingResult.hasErrors()) {
+             if (opstina.getValue()==0) {
+            redirectAttributes.addFlashAttribute("LSGErrorMessage", "Морате одабрати ниво приступа");
+          redirectAttributes.addFlashAttribute("errorMessage", "Грешка приликом креирања клијента!");
+           return "redirect:/admin/clients/new";
+        }else {
+            if (name.isEmpty()) {
+            redirectAttributes.addFlashAttribute("NameErrorMessage", "Морате унети име клијента");
             redirectAttributes.addFlashAttribute("errorMessage", "Грешка приликом креирања клијента!");
-            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.newClient", bindingResult);
-            redirectAttributes.addFlashAttribute("newClient", newClient);
             return "redirect:/admin/clients/new";
-        } else {
-            if (clientService.isNameUsed(newClient.getName())) {
+        }}
+
+            if (clientService.isNameUsed(name)) {
                 redirectAttributes.addFlashAttribute("errorMessage", "Клијент са истим називом већ постоји!");
                 return "redirect:/admin/clients/new";
             } else {
+                
+                //OpstinaID opstinaID=new OpstinaID(opstina);
+                Client newClient= new Client();
+                newClient.setName(name);
+                newClient.setOpstina(opstinaService.findOne(opstina));
                 clientService.createNew(newClient);
                 
                 redirectAttributes.addFlashAttribute("successMessage", "Клијент успешно креиран!");
                 return "redirect:/admin/clients";
             }
-        }
+        
     }
 
     @GetMapping(value = "/clients/{clientId}")
@@ -101,63 +116,58 @@ public class ClientsController {
     @GetMapping(value = "/clients/{clientId}/edit")
     public String edit(
             @PathVariable final ClientId clientId,
-          
             final Model model
-            ) {
+    ) {
         Client client = clientService.findOne(clientId);
-         List<Opstina> opstine = new ArrayList();
-        opstine=opstinaService.findAllOrderByNameAsc();
+        List<Opstina> opstine = new ArrayList();
+        opstine = opstinaService.findAllOrderByNameAsc();
         List<Distrikt> districts = new ArrayList();
-        districts=distriktService.findAllOrderByNameAsc();
+        districts = distriktService.findAllOrderByNameAsc();
         List<Provincija> provincije = new ArrayList();
-        provincije=provincijaService.findAllOrderByNameAsc();
-        
+        provincije = provincijaService.findAllOrderByNameAsc();
+
         model.addAttribute("editClient", client);
         model.addAttribute("opstine", opstine);
         model.addAttribute("districts", districts);
         model.addAttribute("provincije", provincije);
-        
+
         return "admin/clients/edit";
     }
 
     @PostMapping(value = "/clients/{clientId}/edit")
     public String update(
             @PathVariable final ClientId clientId,
-          
             @RequestParam(name = "opstinaId") OpstinaID opstinaId,
-            @RequestParam(name = "opstinaId") ProvincijaID provincijaId,
-            @RequestParam(name = "distriktId") DistriktID distriktID,
             @RequestParam(name = "name") String name,
             @RequestParam(name = "aktivan", required = false) boolean aktivan,
-        
             final RedirectAttributes redirectAttributes,
-            final Model model
-            
-            
-            
-           
-    ) { Client client = clientService.findOne(clientId);
-            client.setName(name);
-           
-               if (aktivan==true){
+            final Model model) {
+        if (opstinaId.getValue()==0) {
+            redirectAttributes.addFlashAttribute("LSGErrorMessage", "Морате одабрати ниво приступа");
+            redirectAttributes.addFlashAttribute("errorMessage", "Грешка приликом креирања клијента!");
+            return "redirect:/admin/clients/{clientId}/edit";
+        } else {
+            if (name.isEmpty()) {
+            redirectAttributes.addFlashAttribute("NameErrorMessage", "Морате унети име клијента");
+            redirectAttributes.addFlashAttribute("errorMessage", "Грешка приликом креирања клијента!");
+            return "redirect:/admin/clients/{clientId}/edit";
+        }
+        Client client = clientService.findOne(clientId);
+        client.setName(name);
+        if (aktivan == true) {
             client.setActive(true);
-            }else{client.setActive(false);}
-           
-               client.setOpstina(opstinaService.findOne(opstinaId));
-               client.getOpstina().setProvincija(provincijaService.findOne(provincijaId));
-               client.getOpstina().setDistrikt(distriktService.findOne(distriktID));
-              try {
-                
+        } else {
+            client.setActive(false);
+        }
+        client.setOpstina(opstinaService.findOne(opstinaId));
+        try {
             clientService.update(clientId, client);
             redirectAttributes.addFlashAttribute("successMessage", "Клијент успешно измењен!");
-            
-            } catch (Exception e) {
+        } catch (Exception e) {
             redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
         }
-        
         return "redirect:/admin/clients/" + clientId;
     }
-    
-
+    }
 
 }

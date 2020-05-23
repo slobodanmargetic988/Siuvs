3914870433b19bf2fb633e83b470464bc54ed48e
slobodanmargetic988 <slobodanmargetic988@gmail.com>
2020-05-23 08:47:16 +0200

    add edit delete PPD IA Tasks

--- src/main/java/slobodan/siuvs2/controller/admin/clients/ClientsController.java
+++ src/main/java/slobodan/siuvs2/controller/admin/clients/ClientsController.java
@@ -15,12 +15,24 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import slobodan.siuvs2.model.Distrikt;
+import slobodan.siuvs2.model.InternationalAgreements;
 import slobodan.siuvs2.model.Opstina;
 import slobodan.siuvs2.model.Provincija;
+import slobodan.siuvs2.model.PublicPolicyDocuments;
+import slobodan.siuvs2.model.Tasks;
 import slobodan.siuvs2.service.DistriktService;
+import slobodan.siuvs2.service.InternationalAgreementsFactory;
+import slobodan.siuvs2.service.InternationalAgreementsService;
 import slobodan.siuvs2.service.OpstinaService;
 import slobodan.siuvs2.service.ProvincijaService;
+import slobodan.siuvs2.service.PublicPolicyDocumentsFactory;
+import slobodan.siuvs2.service.PublicPolicyDocumentsService;
+import slobodan.siuvs2.service.TasksFactory;
+import slobodan.siuvs2.service.TasksService;
+import slobodan.siuvs2.valueObject.InternationalAgreementsID;
 import slobodan.siuvs2.valueObject.OpstinaID;
+import slobodan.siuvs2.valueObject.PublicPolicyDocumentsID;
+import slobodan.siuvs2.valueObject.TasksID;
 
 @Scope(WebApplicationContext.SCOPE_REQUEST)
 @Controller
@@ -35,6 +47,18 @@ public class ClientsController {
     private ProvincijaService provincijaService;
     @Autowired
     private DistriktService distriktService;
+    @Autowired
+    private TasksService tasksService;
+    @Autowired
+    private PublicPolicyDocumentsService PPDService;
+    @Autowired
+    private InternationalAgreementsService IAService;
+    @Autowired
+    private TasksFactory tasksFactory;
+    @Autowired
+    private PublicPolicyDocumentsFactory PPDFactory;
+    @Autowired
+    private InternationalAgreementsFactory IAFactory;            
 
     @ModelAttribute("newClient")
     public Client getClient() {
@@ -138,7 +162,7 @@ public class ClientsController {
             } else {
                 if (clientService.isNameUsed(clientId, name)) {
                     redirectAttributes.addFlashAttribute("errorMessage", "Корисник са истим називом већ постоји!");
-                    return "redirect:/admin/clients/{clientId}/edit";
+                    return "redirect:/admin/clients/"+clientId+"/edit";
                 }
             }
 
@@ -155,10 +179,228 @@ public class ClientsController {
                 redirectAttributes.addFlashAttribute("successMessage", "Корисник успешно измењен!");
             } catch (Exception e) {
                 redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
-                return "redirect:/admin/clients/{clientId}/edit";
+                return "redirect:/admin/clients/"+clientId+"/edit";
             }
             return "redirect:/admin/clients/" + clientId;
         }
     }
-
+    
+    @GetMapping(value = "/clients/{clientId}/newNadleznost")
+    public String newNadleznost(@PathVariable final ClientId clientId, final Model model) {
+        Client client = clientService.findOne(clientId);
+        model.addAttribute("client", client);
+        return "admin/clients/newNadleznost";
+    }
+    @GetMapping(value = "/clients/{clientId}/newIA")
+    public String newIA(@PathVariable final ClientId clientId, final Model model) {
+        Client client = clientService.findOne(clientId);
+        model.addAttribute("client", client);
+        return "admin/clients/newIA";
+    }
+    @GetMapping(value = "/clients/{clientId}/newPPD")
+    public String newPPD(@PathVariable final ClientId clientId, final Model model) {
+        Client client = clientService.findOne(clientId);
+        model.addAttribute("client", client);
+        return "admin/clients/newPPD";
+    }
+    
+    @PostMapping(value = "/clients/{clientId}/newNadleznost")
+    public String savenewNadleznost(@PathVariable final ClientId clientId,
+            @RequestParam(name = "name") String name,
+            final RedirectAttributes redirectAttributes,
+            final Model model) {
+        Client client = clientService.findOne(clientId);
+        Tasks task=tasksFactory.empty(name);
+        try{
+        tasksService.save(task);
+        client.getOpstina().getTasks().add(task);
+        clientService.save(client);
+        redirectAttributes.addFlashAttribute("successMessage", "Надлежност и делокруг успешно додат!");
+            } catch (Exception e) {
+                redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
+                return "redirect:/admin/clients/"+clientId+"/newNadleznost";
+            }
+        return "redirect:/admin/clients/" + clientId;
+    }
+    @PostMapping(value = "/clients/{clientId}/newIA")
+    public String savenewIA(@PathVariable final ClientId clientId,
+            @RequestParam(name = "name") String name,
+            final RedirectAttributes redirectAttributes,
+            final Model model) {
+        Client client = clientService.findOne(clientId);
+        InternationalAgreements ia=IAFactory.empty(name);
+        try{
+        IAService.save(ia);
+        client.getOpstina().getInternationalAgreements().add(ia);
+        clientService.save(client);
+        redirectAttributes.addFlashAttribute("successMessage", "Mеђународни документ успешно додат!");
+            } catch (Exception e) {
+                redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
+                return "redirect:/admin/clients/"+clientId+"/newIA";
+            }
+        return "redirect:/admin/clients/" + clientId;
+    }
+    @PostMapping(value = "/clients/{clientId}/newPPD")
+    public String savenewPPD(@PathVariable final ClientId clientId,
+            @RequestParam(name = "name") String name,
+            final RedirectAttributes redirectAttributes,
+            final Model model) {
+        Client client = clientService.findOne(clientId);
+        PublicPolicyDocuments ppd=PPDFactory.empty(name);
+        try{
+        PPDService.save(ppd);
+        client.getOpstina().getPublicPolicyDocuments().add(ppd);
+        clientService.save(client);
+        redirectAttributes.addFlashAttribute("successMessage", "Jавни документ документ успешно додат!");
+            } catch (Exception e) {
+                redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
+                return "redirect:/admin/clients/"+clientId+"/newPPD";
+            }
+        return "redirect:/admin/clients/" + clientId;
+    }
+        @GetMapping(value = "/clients/{clientId}/deleteNadleznost/{Nadleznostid}")
+    public String deleteNadleznost(@PathVariable final ClientId clientId,
+            @PathVariable final TasksID Nadleznostid,
+            final RedirectAttributes redirectAttributes,
+            final Model model) {
+        Client client = clientService.findOne(clientId);
+        Tasks task=tasksService.findOne(Nadleznostid);
+        try{        
+        client.getOpstina().getTasks().remove(task);
+        clientService.save(client);
+        tasksService.delete(Nadleznostid);
+        redirectAttributes.addFlashAttribute("successMessage", "Надлежност и делокруг успешно уклоњен!");
+            } catch (Exception e) {
+                redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
+                return "redirect:/admin/clients/"+clientId;
+            }
+        return "redirect:/admin/clients/" + clientId;
+    }
+    @GetMapping(value = "/clients/{clientId}/deleteIA/{IAid}")
+    public String deleteIA(@PathVariable final ClientId clientId,
+            @PathVariable final InternationalAgreementsID IAid,
+            final RedirectAttributes redirectAttributes,
+            final Model model) {
+        Client client = clientService.findOne(clientId);
+        InternationalAgreements ia=IAService.findOne(IAid);
+        try{        
+        client.getOpstina().getInternationalAgreements().remove(ia);
+        clientService.save(client);
+        IAService.delete(IAid);
+        redirectAttributes.addFlashAttribute("successMessage", "Mеђународни документ успешно уклоњен!");
+            } catch (Exception e) {
+                redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
+                return "redirect:/admin/clients/"+clientId;
+            }
+        return "redirect:/admin/clients/" + clientId;
+    }
+    
+    @GetMapping(value = "/clients/{clientId}/deletePPD/{PPDid}")
+    public String deletePPD(@PathVariable final ClientId clientId,
+            @PathVariable final PublicPolicyDocumentsID PPDid,
+            final RedirectAttributes redirectAttributes,
+            final Model model) {
+        Client client = clientService.findOne(clientId);
+        PublicPolicyDocuments PPD=PPDService.findOne(PPDid);
+        try{        
+        client.getOpstina().getPublicPolicyDocuments().remove(PPD);
+        clientService.save(client);
+        PPDService.delete(PPDid);
+        redirectAttributes.addFlashAttribute("successMessage", "Jавни документ успешно уклоњен!");
+            } catch (Exception e) {
+                redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
+                return "redirect:/admin/clients/"+clientId;
+            }
+        return "redirect:/admin/clients/" + clientId;
+    }
+    @GetMapping(value = "/clients/{clientId}/editNadleznost/{Nadleznostid}")
+    public String editNadleznost(@PathVariable final ClientId clientId,
+            @PathVariable final TasksID Nadleznostid,           
+            final RedirectAttributes redirectAttributes,
+            final Model model) {
+        Client client = clientService.findOne(clientId);
+        model.addAttribute("client", client);
+        Tasks task=tasksService.findOne(Nadleznostid);
+        model.addAttribute("task", task);
+        return "admin/clients/editNadleznost";
+    }
+    
+    @PostMapping(value = "/clients/{clientId}/editNadleznost/{Nadleznostid}")
+    public String saveEditNadleznost(@PathVariable final ClientId clientId,
+            @PathVariable final TasksID Nadleznostid,
+            @RequestParam(name = "name") String name,
+            final RedirectAttributes redirectAttributes,
+            final Model model) {
+        Client client = clientService.findOne(clientId);
+        Tasks task=tasksService.findOne(Nadleznostid);
+        try{        
+        task.setName(name);       
+        tasksService.save(task);
+        redirectAttributes.addFlashAttribute("successMessage", "Надлежност и делокруг успешно измењен!");
+            } catch (Exception e) {
+                redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
+                return "redirect:/admin/clients/"+clientId;
+            }
+        return "redirect:/admin/clients/" + clientId;
+    }
+        @GetMapping(value = "/clients/{clientId}/editIA/{IAid}")
+    public String editIA(@PathVariable final ClientId clientId,
+            @PathVariable final InternationalAgreementsID IAid,           
+            final RedirectAttributes redirectAttributes,
+            final Model model) {
+        Client client = clientService.findOne(clientId);
+        model.addAttribute("client", client);
+        InternationalAgreements IA=IAService.findOne(IAid);
+        model.addAttribute("IA", IA);
+        return "admin/clients/editIA";
+    }
+    
+    @PostMapping(value = "/clients/{clientId}/editIA/{IAid}")
+    public String saveEditIA(@PathVariable final ClientId clientId,
+            @PathVariable final InternationalAgreementsID IAid,
+            @RequestParam(name = "name") String name,
+            final RedirectAttributes redirectAttributes,
+            final Model model) {
+        Client client = clientService.findOne(clientId);
+        InternationalAgreements IA=IAService.findOne(IAid);
+        try{        
+        IA.setName(name);       
+        IAService.save(IA);
+        redirectAttributes.addFlashAttribute("successMessage", "Међународни документ успешно измењен!");
+            } catch (Exception e) {
+                redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
+                return "redirect:/admin/clients/"+clientId;
+            }
+        return "redirect:/admin/clients/" + clientId;
+    }
+    @GetMapping(value = "/clients/{clientId}/editPPD/{PPDid}")
+    public String editPPD(@PathVariable final ClientId clientId,
+            @PathVariable final PublicPolicyDocumentsID PPDid,           
+            final RedirectAttributes redirectAttributes,
+            final Model model) {
+        Client client = clientService.findOne(clientId);
+        model.addAttribute("client", client);
+        PublicPolicyDocuments PPD=PPDService.findOne(PPDid);
+        model.addAttribute("PPD", PPD);
+        return "admin/clients/editPPD";
+    }
+    
+    @PostMapping(value = "/clients/{clientId}/editPPD/{PPDid}")
+    public String saveeditPPD(@PathVariable final ClientId clientId,
+            @PathVariable final PublicPolicyDocumentsID PPDid,
+            @RequestParam(name = "name") String name,
+            final RedirectAttributes redirectAttributes,
+            final Model model) {
+        Client client = clientService.findOne(clientId);
+        PublicPolicyDocuments PPD=PPDService.findOne(PPDid);
+        try{        
+        PPD.setName(name);       
+        PPDService.save(PPD);
+        redirectAttributes.addFlashAttribute("successMessage", "Јавни документ успешно измењен!");
+            } catch (Exception e) {
+                redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
+                return "redirect:/admin/clients/"+clientId;
+            }
+        return "redirect:/admin/clients/" + clientId;
+    }
 }
--- src/main/java/slobodan/siuvs2/controller/client/OverviewController.java
+++ src/main/java/slobodan/siuvs2/controller/client/OverviewController.java
@@ -10,12 +10,29 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import slobodan.siuvs2.model.Client;
+import slobodan.siuvs2.model.InternationalAgreements;
+import slobodan.siuvs2.model.PublicPolicyDocuments;
 import slobodan.siuvs2.model.SiuvsUserPrincipal;
+import slobodan.siuvs2.model.Tasks;
 import slobodan.siuvs2.model.User;
 import slobodan.siuvs2.service.ClientService;
+import slobodan.siuvs2.service.InternationalAgreementsFactory;
+import slobodan.siuvs2.service.InternationalAgreementsService;
+import slobodan.siuvs2.service.PublicPolicyDocumentsFactory;
+import slobodan.siuvs2.service.PublicPolicyDocumentsService;
+import slobodan.siuvs2.service.TasksFactory;
+import slobodan.siuvs2.service.TasksService;
+import slobodan.siuvs2.valueObject.ClientId;
+import slobodan.siuvs2.valueObject.InternationalAgreementsID;
+import slobodan.siuvs2.valueObject.PublicPolicyDocumentsID;
+import slobodan.siuvs2.valueObject.TasksID;
 
 /**
  *
@@ -28,15 +45,255 @@ public class OverviewController {
 
     @Autowired
     private ClientService clientService;
+    @Autowired
+    private TasksService tasksService;
+    @Autowired
+    private PublicPolicyDocumentsService PPDService;
+    @Autowired
+    private InternationalAgreementsService IAService;
+    @Autowired
+    private TasksFactory tasksFactory;
+    @Autowired
+    private PublicPolicyDocumentsFactory PPDFactory;
+    @Autowired
+    private InternationalAgreementsFactory IAFactory;
 
     @GetMapping(value = "/overview")
     public String list(final Model model) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        User currentUser = ((SiuvsUserPrincipal) authentication.getPrincipal()).getUser();
-        Client client = currentUser.getClient();
+        User currentUser = ((SiuvsUserPrincipal) authentication.getPrincipal()).getUser();       
+        Client client=clientService.findOne(currentUser.getClient().getClientId());
 
         model.addAttribute("client", clientService.findOne(client.getClientId()));
         return "client/overview/view";
     }
 
+
+    @GetMapping(value = "/overview/newNadleznost")
+    public String newNadleznost( final Model model) {
+        return "/client/overview/newNadleznost";
+    }
+    @GetMapping(value = "/overview/newIA")
+    public String newIA( final Model model) {
+        
+        return "/client/overview/newIA";
+    }
+    @GetMapping(value = "/overview/newPPD")
+    public String newPPD( final Model model) {
+       
+        return "/client/overview/newPPD";
+    }
+    
+    @PostMapping(value = "/overview/newNadleznost")
+    public String savenewNadleznost(
+            @RequestParam(name = "name") String name,
+            final RedirectAttributes redirectAttributes,
+            final Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User currentUser = ((SiuvsUserPrincipal) authentication.getPrincipal()).getUser();       
+        Client client=clientService.findOne(currentUser.getClient().getClientId());
+        Tasks task=tasksFactory.empty(name);
+        try{
+        tasksService.save(task);
+        client.getOpstina().getTasks().add(task);
+        clientService.save(client);
+        redirectAttributes.addFlashAttribute("successMessage", "Надлежност и делокруг успешно додат!");
+            } catch (Exception e) {
+                redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
+                return "redirect:/client/overview/newNadleznost";
+            }
+        return "redirect:/client/overview";
+    }
+    @PostMapping(value = "/overview/newIA")
+    public String savenewIA(
+            @RequestParam(name = "name") String name,
+            final RedirectAttributes redirectAttributes,
+            final Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User currentUser = ((SiuvsUserPrincipal) authentication.getPrincipal()).getUser();       
+        Client client=clientService.findOne(currentUser.getClient().getClientId());
+        
+        InternationalAgreements ia=IAFactory.empty(name);
+        try{
+        IAService.save(ia);
+        client.getOpstina().getInternationalAgreements().add(ia);
+        clientService.save(client);
+        redirectAttributes.addFlashAttribute("successMessage", "Mеђународни документ успешно додат!");
+            } catch (Exception e) {
+                redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
+                return "redirect:/client/overview/newIA";
+            }
+        return "redirect:/client/overview";
+    }
+    @PostMapping(value = "/overview/newPPD")
+    public String savenewPPD(
+            @RequestParam(name = "name") String name,
+            final RedirectAttributes redirectAttributes,
+            final Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User currentUser = ((SiuvsUserPrincipal) authentication.getPrincipal()).getUser();       
+        Client client=clientService.findOne(currentUser.getClient().getClientId());
+        PublicPolicyDocuments ppd=PPDFactory.empty(name);
+        try{
+        PPDService.save(ppd);
+        client.getOpstina().getPublicPolicyDocuments().add(ppd);
+        clientService.save(client);
+        redirectAttributes.addFlashAttribute("successMessage", "Jавни документ документ успешно додат!");
+            } catch (Exception e) {
+                redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
+                return "redirect:/client/overview/newPPD";
+            }
+        return "redirect:/client/overview";
+    }
+    @GetMapping(value = "/overview/deleteNadleznost/{Nadleznostid}")
+    public String deleteNadleznost(
+            @PathVariable final TasksID Nadleznostid,
+            final RedirectAttributes redirectAttributes,
+            final Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User currentUser = ((SiuvsUserPrincipal) authentication.getPrincipal()).getUser();       
+        Client client=clientService.findOne(currentUser.getClient().getClientId());
+        Tasks task=tasksService.findOne(Nadleznostid);
+        try{        
+        client.getOpstina().getTasks().remove(task);
+        clientService.save(client);
+        tasksService.delete(Nadleznostid);
+        redirectAttributes.addFlashAttribute("successMessage", "Надлежност и делокруг успешно уклоњен!");
+            } catch (Exception e) {
+                redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
+                return "redirect:/client/overview";
+            }
+        return "redirect:/client/overview";
+    }
+    @GetMapping(value = "/overview/deleteIA/{IAid}")
+    public String deleteIA(
+            @PathVariable final InternationalAgreementsID IAid,
+            final RedirectAttributes redirectAttributes,
+            final Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User currentUser = ((SiuvsUserPrincipal) authentication.getPrincipal()).getUser();       
+        Client client=clientService.findOne(currentUser.getClient().getClientId());
+        InternationalAgreements ia=IAService.findOne(IAid);
+        try{        
+        client.getOpstina().getInternationalAgreements().remove(ia);
+        clientService.save(client);
+        IAService.delete(IAid);
+        redirectAttributes.addFlashAttribute("successMessage", "Mеђународни документ успешно уклоњен!");
+            } catch (Exception e) {
+                redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
+                return "redirect:/client/overview";
+            }
+        return "redirect:/client/overview";
+    }
+    
+    @GetMapping(value = "/overview/deletePPD/{PPDid}")
+    public String deletePPD(
+            @PathVariable final PublicPolicyDocumentsID PPDid,
+            final RedirectAttributes redirectAttributes,
+            final Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User currentUser = ((SiuvsUserPrincipal) authentication.getPrincipal()).getUser();       
+        Client client=clientService.findOne(currentUser.getClient().getClientId());
+        PublicPolicyDocuments PPD=PPDService.findOne(PPDid);
+        try{        
+        client.getOpstina().getPublicPolicyDocuments().remove(PPD);
+        clientService.save(client);
+        PPDService.delete(PPDid);
+        redirectAttributes.addFlashAttribute("successMessage", "Jавни документ успешно уклоњен!");
+            } catch (Exception e) {
+                redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
+                return "redirect:/client/overview";
+            }
+        return "redirect:/client/overview";
+    }
+    
+    
+      @GetMapping(value = "/overview/editNadleznost/{Nadleznostid}")
+    public String editNadleznost(
+            @PathVariable final TasksID Nadleznostid,           
+            final RedirectAttributes redirectAttributes,
+            final Model model) {
+        Tasks task=tasksService.findOne(Nadleznostid);
+        model.addAttribute("task", task);
+        return "/client/overview/editNadleznost";
+    }
+    
+        @GetMapping(value = "/overview/editIA/{IAid}")
+    public String editIA(
+            @PathVariable final InternationalAgreementsID IAid,
+            final RedirectAttributes redirectAttributes,
+            final Model model) {
+        InternationalAgreements IA = IAService.findOne(IAid);
+        model.addAttribute("IA", IA);
+        return "/client/overview/editIA";
+    }
+    @GetMapping(value = "/overview/editPPD/{PPDid}")
+    public String editPPD(
+            @PathVariable final PublicPolicyDocumentsID PPDid,           
+            final RedirectAttributes redirectAttributes,
+            final Model model) {
+        PublicPolicyDocuments PPD=PPDService.findOne(PPDid);
+        model.addAttribute("PPD", PPD);
+        return "/client/overview/editPPD";
+    }
+    @PostMapping(value = "/overview/editNadleznost/{Nadleznostid}")
+    public String saveEditNadleznost(
+            @PathVariable final TasksID Nadleznostid,
+            @RequestParam(name = "name") String name,
+            final RedirectAttributes redirectAttributes,
+            final Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User currentUser = ((SiuvsUserPrincipal) authentication.getPrincipal()).getUser();       
+        Client client=clientService.findOne(currentUser.getClient().getClientId());
+        Tasks task=tasksService.findOne(Nadleznostid);
+        try{        
+        task.setName(name);       
+        tasksService.save(task);
+        redirectAttributes.addFlashAttribute("successMessage", "Надлежност и делокруг успешно измењен!");
+            } catch (Exception e) {
+                redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
+                return "redirect:/client/overview";
+            }
+        return "redirect:/client/overview";
+    }
+    @PostMapping(value = "/overview/editIA/{IAid}")
+    public String saveEditIA(
+            @PathVariable final InternationalAgreementsID IAid,
+            @RequestParam(name = "name") String name,
+            final RedirectAttributes redirectAttributes,
+            final Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User currentUser = ((SiuvsUserPrincipal) authentication.getPrincipal()).getUser();       
+        Client client=clientService.findOne(currentUser.getClient().getClientId());
+        InternationalAgreements IA=IAService.findOne(IAid);
+        try{        
+        IA.setName(name);       
+        IAService.save(IA);
+        redirectAttributes.addFlashAttribute("successMessage", "Међународни документ успешно измењен!");
+            } catch (Exception e) {
+                redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
+                return "redirect:/client/overview";
+            }
+        return "redirect:/client/overview";
+    }
+    @PostMapping(value = "/overview/editPPD/{PPDid}")
+    public String saveeditPPD(
+            @PathVariable final PublicPolicyDocumentsID PPDid,
+            @RequestParam(name = "name") String name,
+            final RedirectAttributes redirectAttributes,
+            final Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User currentUser = ((SiuvsUserPrincipal) authentication.getPrincipal()).getUser();       
+        Client client=clientService.findOne(currentUser.getClient().getClientId());
+        PublicPolicyDocuments PPD=PPDService.findOne(PPDid);
+        try{        
+        PPD.setName(name);       
+        PPDService.save(PPD);
+        redirectAttributes.addFlashAttribute("successMessage", "Јавни документ успешно измењен!");
+            } catch (Exception e) {
+                redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
+                return "redirect:/client/overview";
+            }
+        return "redirect:/client/overview";
+    }
 }

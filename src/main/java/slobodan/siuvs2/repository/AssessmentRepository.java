9baebb445ad8dd4fabd5996c3e2b09be0368152d
slobodanmargetic988 <slobodanmargetic988@gmail.com>
2022-03-07 23:52:45 +0100

    Update AssessmentRepository.java

--- src/main/java/slobodan/siuvs2/repository/AssessmentRepository.java
+++ src/main/java/slobodan/siuvs2/repository/AssessmentRepository.java
@@ -16,7 +16,7 @@ public interface AssessmentRepository extends JpaRepository<Assessment, Integer>
     Assessment findFirstByClientAndPage(Client client, Page page);
     Assessment findFirstByClient(Client client);
     
-    @Query(value = "SELECT assessment_id, client_id, page_id, consequences, probability, text_pojavljivanje, text_prostorna_dimenzija, text_intenzitet, text_vreme, text_tok, text_trajanje, text_rana_najava, text_pripremljenost, text_uticaj, text_generisanje_drugih_opasnosti, text_referentni_incidenti, text_informisanje_javnosti, text_buduce_informacije  from `assessment`")
+    @Query(value = "SELECT assessment_id, client_id, page_id, consequences, probability, text_pojavljivanje, text_prostorna_dimenzija, text_intenzitet, text_vreme, text_tok, text_trajanje, text_rana_najava, text_pripremljenost, text_uticaj, text_generisanje_drugih_opasnosti, text_referentni_incidenti, text_informisanje_javnosti, text_buduce_informacije  from assessment", nativeQuery = true)
     List<Object[]> vratisveskraceno();
     
     
